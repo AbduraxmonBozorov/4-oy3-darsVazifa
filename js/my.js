e8f6a6d4daeeb72a04e1287cb7809f4a175f1682
AbduraxmonBozorov <abduraxmonbozorov1999@gmail.com>
2024-05-21 15:16:43 +0500

    4-oy 3-dars vazifasi to'liq bajarildi

---
+++ js/my.js
@@ -0,0 +1,161 @@
+// 1-masala
+// let a = +prompt("Musbat son kiriting");
+// if (a >= 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 2-masala
+// let a = +prompt("Toq son kiriting");
+// if (a % 2 == 1) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 3-masala
+// let a = +prompt("Juft son kiriting");
+// if (a%2==0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 4-masala
+// let A=+prompt("A sonni kiriting");
+// let B=+prompt("B sonni kiriting");
+// if(A>2 && B<=3){
+//     console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+
+// 5-masala
+// let A = +prompt("A sonni kiriting");
+// let B = +prompt("B sonni kiriting");
+// if (A >= 0 || B < -2) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 6-masala
+// let A = +prompt("A sonni kiriting");
+// let B = +prompt("B sonni kiriting");
+// let C = +prompt("C sonni kiriting");
+// if (A<=B && B<=C) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 7-masala
+// let A = +prompt("A sonni kiriting");
+// let B = +prompt("B sonni kiriting");
+// let C = +prompt("C sonni kiriting");
+// if (A<B && B<C) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 8-masala
+// let A = +prompt("A sonni kiriting");
+// let B = +prompt("B sonni kiriting");
+// if (A % 2 == 1 && B % 2 == 1) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+
+// 9-masala
+// let A = +prompt("A sonni kiriting");
+// let B = +prompt("B sonni kiriting");
+// if (A % 2 == 1 || B % 2 == 1) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+
+// 10-masala
+// let A = +prompt("A sonni kiriting");
+// let B = +prompt("B sonni kiriting");
+// if ((A%2==0 && B%2==1) || (A%2==1 && B%2==0)) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+
+// 11-masala
+// let A = +prompt("A sonni kiriting");
+// let B = +prompt("B sonni kiriting");
+// if ((A % 2 == 1 && B % 2 == 1) || (A % 2 == 0 && B % 2 == 0)) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+
+// 12-masala
+// let A = +prompt("A sonni kiriting");
+// let B = +prompt("B sonni kiriting");
+// let C = +prompt("C sonni kiriting");
+
+// if (A >= 0 && B >= 0 && C >= 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 13-masala
+// let A = +prompt("A sonni kiriting");
+// let B = +prompt("B sonni kiriting");
+// let C = +prompt("C sonni kiriting");
+
+// if (A >= 0 || B >= 0 || C >= 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 14-masala
+// let A = +prompt("A sonni kiriting");
+// let B = +prompt("B sonni kiriting");
+// let C = +prompt("C sonni kiriting");
+// if (
+//   (A >= 0 && B < 0 && C < 0) ||
+//   (A < 0 && B >= 0 && C < 0) ||
+//   (A < 0 && B < 0 && C >= 0)
+// ) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// 15-masala
+// let A = +prompt("A sonni kiriting");
+// let B = +prompt("B sonni kiriting");
+// let C = +prompt("C sonni kiriting");
+
+// if (A >= 0 && B >= 0 && C < 0) {
+//   console.log(true);
+// } else if (A >= 0 && B < 0 && C >= 0) {
+//   console.log(true);
+// } else if (A < 0 && B >= 0 && C >= 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// // 16-masala
+// let A = +prompt("Son kiriting");
+// if (A % 2 == 0 && (A > 9 && A < 100)) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// // 17-masala: Berilgan son TOQ yoki [-10; -100] oraliqda bo'lsin
+// let A = +prompt("Son kiriting");
+// if (A % 2 == 1 || (A <= -10 && A >= -100)) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
